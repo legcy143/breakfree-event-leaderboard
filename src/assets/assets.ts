f0b6dfb8c1ad6d297b061e8b73cb238e8a97c898
prince <legcy143@gmail.com>
2025-05-23 15:57:18 +0530

    update assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,4 +1,4 @@
 export const assets = {
-    background :"https://gkh-images.s3.amazonaws.com/7cb2ee3b-adc2-4de8-84d3-4b8eb7509f2a_Background.png",
-    logo :"https://gkh-images.s3.amazonaws.com/f647b31e-3e37-4751-9c66-482b96b2d6ec_Breakfree%20logo.png",
+    background :"https://gkh-images.s3.amazonaws.com/ff344689-9779-4e59-87be-d5914a3f3e13_background.png",
+    logo :"https://gkh-images.s3.amazonaws.com/8ed0f12f-169c-41be-8427-9022c6fe3c95_logo.png",
 }
