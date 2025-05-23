dac9c36ba71ca4ae314bde17e931fd97e619f07f
prince <legcy143@gmail.com>
2025-05-23 17:05:33 +0530

    fix: remove all users

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -6,24 +6,5 @@ export type Team = {
 
 // Export the teams data for use across the application
 export const teamsData: Team[] = [
-  {
-    name: 'Shivam',
-    score: '00',
-  },
-  {
-    name: 'Shivam',
-    score: '00',
-  },
-  {
-    name: 'Shivam',
-    score: '00',
-  },
-  {
-    name: 'Shivam',
-    score: '00',
-  },
-  {
-    name: 'Mayank',
-    score: '00',
-  },
+  
 ]
