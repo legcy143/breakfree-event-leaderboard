fde36ea290b98b4b7827f9018bf09227f2cc7b4e
prince <legcy143@gmail.com>
2025-06-05 14:15:49 +0530

    fix: logo

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,4 +1,4 @@
 export const assets = {
     background :"https://gkh-images.s3.amazonaws.com/ff344689-9779-4e59-87be-d5914a3f3e13_background.png",
-    logo :"https://gkh-images.s3.amazonaws.com/8ed0f12f-169c-41be-8427-9022c6fe3c95_logo.png",
+    logo :"https://gkh-images.s3.amazonaws.com/ff101abf-5e52-49c4-aec9-2746884ebdae_logo-abcd.png",
 }
